faf64a29e2e9e8ec14975b89dda4d9a9f1e8e8f4
Stephen Avino <stephen.avino@gmail.com>
2021-11-02 11:12:04 -0400

    commit tree 936da55bb81be7fd9e4eef847aa52332e1977fc6

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+let distance;
+function distanceFromHqInBlocks(block){
+    if(block>=42){
+        distance = (block - 42);
+}
+    if(block<42){
+        distance = (42 - block);
+        }
+return distance;}
+
+function distanceFromHqInFeet(block){
+   return distanceFromHqInBlocks(block) * 264
+};
+
+function distanceTravelledInFeet(block1, block2){
+    return Math.abs((block1 - block2) * 264);
+}
+let feet;
+function calculatesFarePrice(block1, block2){
+    feet = Math.abs((block1 - block2) * 264);
+    if(feet<400)
+        return 0;
+    if (feet<=2000)
+        return((feet-400) * .02);
+    if (feet<=2500)
+        return(25);
+    if (feet>2500)
+        return `cannot travel that far`;
+    }
